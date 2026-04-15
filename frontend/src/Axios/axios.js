df025769090f2e3216c84b8baab8fa302d47a075
mihiragath <mihiragath@users.noreply.github.com>
2026-04-15 16:53:40 +0530

    Update axios instance to use environment variable for baseURL

--- frontend/src/Axios/axios.js
+++ frontend/src/Axios/axios.js
@@ -1,5 +1,9 @@
 import axios from "axios";
+
 const instance = axios.create({
-  baseURL: "https://mern-todo-app-gh2q.onrender.com/api",
+  baseURL:
+    process.env.REACT_APP_API_URL ||
+    "https://mern-todo-app-gh2q.onrender.com/api",
 });
+
 export default instance;
